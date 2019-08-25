45dc5d5e8ea58819543d432605e016c2ad49cf0b
ChapmanApples <102115618@student.swin.edu.au>
2019-08-25 14:50:35 +1000

    Added Agent Behaviour

---
+++ VehicleRoutingProblem/src/RoutingAgents/MasterRoutingAgent.java
@@ -0,0 +1,10 @@
+package RoutingAgents;
+
+import jade.core.Agent;
+
+@SuppressWarnings("serial")
+public class MasterRoutingAgent extends Agent{
+	protected void setup() {
+		System.out.println("Hello world!");
+	}
+}
