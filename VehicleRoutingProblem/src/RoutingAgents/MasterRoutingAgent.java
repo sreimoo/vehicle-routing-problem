2f52187ffa5fb27219f7b66a6cbf289d61037b5d
ChapmanApples <102115618@student.swin.edu.au>
2019-08-29 13:50:21 +1000

    Updated Master Agent to have AMS behaviours

--- VehicleRoutingProblem/src/RoutingAgents/MasterRoutingAgent.java
+++ VehicleRoutingProblem/src/RoutingAgents/MasterRoutingAgent.java
@@ -1,13 +1,30 @@
 package RoutingAgents;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.AMSAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
 
 @SuppressWarnings("serial")
 public class MasterRoutingAgent extends Agent{
 	protected void setup() {
-		Object[] args = getArguments();
-		for(int i = 0; i < args.length; i++) {
-			System.out.println(args[i]);
-		}
+		AMSAgentDescription [] agents = null;        
+		try {             
+			SearchConstraints c = new SearchConstraints();             
+			c.setMaxResults ((long) -1);   
+				agents = AMSService.search( this, new AMSAgentDescription(), c );   
+				}   
+				catch (Exception e) {             
+				System.out.println( "Problem searching AMS: " + e );             
+				e.printStackTrace();   
+				}      
+			AID myID = getAID();   
+			for (int i=0; i<agents.length;i++)   {    
+				AID agentID = agents[i].getName();    
+				System.out.println(     
+						( agentID.equals( myID ) ? "*** " : "    ")        
+						+ i + ": " + agentID.getName());   
+			}  
 	}
 }
