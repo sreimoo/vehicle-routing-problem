2bee87dbb129e2d4564be13f2f11e210a3abde60
ChapmanApples <102115618@student.swin.edu.au>
2019-09-05 08:58:15 +1000

    Updated MRA to communicate with DAs

--- VehicleRoutingProblem/src/RoutingAgents/MasterRoutingAgent.java
+++ VehicleRoutingProblem/src/RoutingAgents/MasterRoutingAgent.java
@@ -2,12 +2,16 @@ package RoutingAgents;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.AMSService;
+import jade.lang.acl.ACLMessage;
 import jade.domain.FIPAAgentManagement.AMSAgentDescription;
 import jade.domain.FIPAAgentManagement.SearchConstraints;
 
 @SuppressWarnings("serial")
 public class MasterRoutingAgent extends Agent{
+	int agentCount = 0;
+	
 	protected void setup() {
 		AMSAgentDescription [] agents = null;        
 		try {             
@@ -19,12 +23,42 @@ public class MasterRoutingAgent extends Agent{
 				System.out.println( "Problem searching AMS: " + e );             
 				e.printStackTrace();   
 				}      
-			AID myID = getAID();   
+			AID da1 = getAID("DeliveryAgent1");
+			AID da2 = getAID("DeliveryAgent2");
+			AID da3 = getAID("DeliveryAgent3");
 			for (int i=0; i<agents.length;i++)   {    
-				AID agentID = agents[i].getName();    
+				AID agentID = agents[i].getName(); 
+				if(agentID.equals(da1) || agentID.equals(da2) || agentID.equals(da3)) {
+					agentCount ++;
+				}
+				//agentCount ++;
 				System.out.println(     
-						( agentID.equals( myID ) ? "*** " : "    ")        
-						+ i + ": " + agentID.getName());   
-			}  
+						( agentID.equals(da1) || agentID.equals(da2) || agentID.equals(da3) ? "*** " : "    ")        
+						+ i + ": " + agentID.getName());
+				}
+			if(agentCount > 0) {
+				System.out.println("We have " + agentCount + " Delivery Agents");
+			}
+			
+			CyclicBehaviour msgReceivingBehaviour =  (new CyclicBehaviour(this)    {  
+			public void action() {       
+				System.out.println(getLocalName() + ": Waiting for Capacity Constraints");  
+				for(int i = 0; i < agentCount; i++) {	
+				ACLMessage msg= receive();     
+				if (msg!=null) {      
+					// Print out message content      
+					System.out.println(getLocalName()+ ": Received Capacity Constraints: " + msg.getContent() 
+					+ " from " + msg.getSender().getLocalName());      
+					}  
+				}
+				// Continue listening //    
+				block();    
+				// This line gets printed since the blocking effect is achieved only after the action() method returns     
+				//System.out.println(getLocalName() + ": This line is printed");     
+				}
+			} );
+			
+			addBehaviour(msgReceivingBehaviour);
 	}
+	
 }
