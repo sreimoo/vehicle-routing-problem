eff828c07f5b5402c425b847d1efa9047c1b5b87
ChapmanApples <102115618@student.swin.edu.au>
2019-09-05 10:47:29 +1000

    Added Node Class

---
+++ VehicleRoutingProblem/src/RoutingAgents/Node.java
@@ -0,0 +1,13 @@
+package RoutingAgents;
+
+public class Node {
+	public String name;
+	public int x_pos;
+	public int y_pos;
+	
+	public Node(String Name, int X_pos, int Y_pos) {
+		name = Name;
+		x_pos = X_pos;
+		y_pos = Y_pos;
+	}
+}
